d9afb74b12c0f3bb0de1f341fa2d760796bd2de7
Ethan Crann <ecrann@protonmail.com>
2026-03-07 13:17:33 -0330

    Depth Meter working. Next step is zoning

--- main.gd
+++ main.gd
@@ -14,25 +14,26 @@ var env: Environment
 var fog_mat: ShaderMaterial
 
 func _ready() -> void:
-	env = world_env.environment
-	env.volumetric_fog_density = 0.0
+	#env = world_env.environment
+	#env.volumetric_fog_density = 0.0
 
-	var surface_y := wader.global_position.y
-	var fog_shader := load("res://fog.gdshader") as Shader
-	fog_mat = ShaderMaterial.new()
-	fog_mat.shader = fog_shader
-	fog_mat.set_shader_parameter("surface_y", surface_y)
-	fog_mat.set_shader_parameter("max_depth", surface_y)
-	fog_mat.set_shader_parameter("fog_color", Color(0.02, 0.08, 0.15, 1.0))
-
-	var fog_volume := FogVolume.new()
-	fog_volume.size = Vector3(1000, surface_y + 100, 1000)
-	fog_volume.position = Vector3(0, surface_y / 2.0, 0)
-	fog_volume.material = fog_mat
-	add_child(fog_volume)
+	#var surface_y := wader.global_position.y
+	#var fog_shader := load("res://fog.gdshader") as Shader
+	#fog_mat = ShaderMaterial.new()
+	#fog_mat.shader = fog_shader
+	#fog_mat.set_shader_parameter("surface_y", surface_y)
+	#fog_mat.set_shader_parameter("max_depth", surface_y)
+	#fog_mat.set_shader_parameter("fog_color", Color(0.02, 0.08, 0.15, 1.0))
+#
+	#var fog_volume := FogVolume.new()
+	#fog_volume.size = Vector3(1000, surface_y + 100, 1000)
+	#fog_volume.position = Vector3(0, surface_y / 2.0, 0)
+	#fog_volume.material = fog_mat
+	#add_child(fog_volume)
+	pass
 
 func _process(_delta: float) -> void:
-	fog_mat.set_shader_parameter("player_pos", player.global_position)
+	#fog_mat.set_shader_parameter("player_pos", player.global_position)
 
 	var dist_to_surface := wader.global_position.y - player.global_position.y
 	var t = clamp((dist_to_surface - ior_dist_min) / (ior_dist_max - ior_dist_min), 0.0, 1.0)
